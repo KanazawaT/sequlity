5c75a668024ef175ec77c3b0fb45df97f458e689
KnzwaT <m1m23a08@oit.ac.jp>
2024-07-07 06:01:26 +0900

    makeKadadiCode

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,33 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public double average(int x, int y) {
+    return (x + y) / 2.0;
+  }
+
+  public int sigma(int x, int y) {
+    return y * (x + y) / 2;
+  }
+
+  public int odd(int x, int y) {
+    int sum = 0;
+    for (int i = x; i <= y; i++) {
+      if (i % 2 != 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  public int even(int x, int y) {
+    int sum = 0;
+    for (int i = x; i <= y; i++) {
+      if (i % 2 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
 }
